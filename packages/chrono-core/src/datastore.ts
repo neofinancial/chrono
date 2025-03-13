aa34f4a2d8fdc0ffd15293c8a524053898a32ff0
Jeremiah Macariola <jeremiah.macariola@neofinancial.com>
2025-03-13 16:17:40 -0600

    Task field desc

--- packages/chrono-core/src/datastore.ts
+++ packages/chrono-core/src/datastore.ts
@@ -1,10 +1,15 @@
 export type TaskStatus = 'pending' | 'claimed' | 'completed' | 'failed';
 
 export type Task<TaskKind, TaskData> = {
+  /** A unique identifier for the task */
   id: string;
+  /** A human-readable name or type for the task */
   kind: TaskKind;
+  /** The current status of the task */
   status: TaskStatus;
+  /** The payload or data associated with the task */
   data: TaskData;
+  /** The current scheduled execution date, which may change if rescheduled */
   scheduledAt: Date;
 };
 
