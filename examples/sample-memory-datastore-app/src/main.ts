10ec269a9c81b6a1a029bbb3eace4cec7f259f5d
Darren Picard <darren.picard@neofinancial.com>
2025-03-18 15:18:05 -0600

    Update example library using register task handler

--- examples/sample-memory-datastore-app/src/main.ts
+++ examples/sample-memory-datastore-app/src/main.ts
@@ -16,6 +16,20 @@ async function main() {
     someField: 123,
   };
 
+  chrono.registerTaskHandler({
+    kind: 'async-messaging',
+    handler: async (task) => {
+      console.log('async-messaging task handler:', task);
+    },
+  });
+
+  chrono.registerTaskHandler({
+    kind: 'send-email',
+    handler: async (task) => {
+      console.log('send-email task handler:', task);
+    },
+  });
+
   const result = await chrono.scheduleTask({
     when: new Date(),
     kind: 'async-messaging',
@@ -23,6 +37,16 @@ async function main() {
   });
 
   console.log('scheduled task:', result);
+
+  const result2 = await chrono.scheduleTask({
+    when: new Date(),
+    kind: 'send-email',
+    data: {
+      url: 'https://example.com',
+    },
+  });
+
+  console.log('scheduled task:', result2);
 }
 
 main().catch(console.error);
