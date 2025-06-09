91ac72773defd37eda15b0e1bd28720afe39c948
Darren Picard <43220734+darrenpicard25@users.noreply.github.com>
2025-06-09 14:15:03 -0600

    Added strongly typed processor events from processor classes (#44)
    Introducing strongly typed Processor EventEmitter types. I do not address all the possible event types that can be emitted from a Processor. Just set up the main 3 for now. Maybe we can talk about convention we want to use for the event types then i can update and hook up code i commented out

--- packages/chrono-core/src/processors/processor.ts
+++ packages/chrono-core/src/processors/processor.ts
@@ -1,6 +1,16 @@
 import type { EventEmitter } from 'node:stream';
+import type { Task, TaskMappingBase } from '..';
 
-export interface Processor extends EventEmitter {
+export type ProcessorEvents<TaskKind extends keyof TaskMapping, TaskMapping extends TaskMappingBase> = {
+  'task:claimed': [{ task: Task<TaskKind, TaskMapping[TaskKind]>; timestamp: Date }];
+  'task:completed': [{ task: Task<TaskKind, TaskMapping[TaskKind]>; timestamp: Date }];
+  'task:failed': [{ task: Task<TaskKind, TaskMapping[TaskKind]>; error: Error; timestamp: Date }];
+  'task:unclaimed': [{ task: Task<TaskKind, TaskMapping[TaskKind]>; error: Error; timestamp: Date }];
+  'task:completion:failed': [{ task: Task<TaskKind, TaskMapping[TaskKind]>; error: unknown; timestamp: Date }];
+};
+
+export interface Processor<TaskKind extends keyof TaskMapping, TaskMapping extends TaskMappingBase>
+  extends EventEmitter<ProcessorEvents<TaskKind, TaskMapping>> {
   start(): Promise<void>;
   stop(): Promise<void>;
 }
