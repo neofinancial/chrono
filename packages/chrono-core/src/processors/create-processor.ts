2f9df70f8d4c07509bda4d4785ce56e33fb1991e
Darren Picard <darren.picard@neofinancial.com>
2025-09-19 11:52:19 -0600

    Added some documentation around chrono processing configuration to help with people attempting to implement it

--- packages/chrono-core/src/processors/create-processor.ts
+++ packages/chrono-core/src/processors/create-processor.ts
@@ -4,13 +4,23 @@ import { type BackoffStrategyOptions, backoffStrategyFactory } from '../backoff-
 import type { Processor } from './processor';
 import { SimpleProcessor } from './simple-processor';
 
+/**
+ * Configuration for the processor.
+ */
 export type ProcessorConfiguration = {
+  /** The maximum number of concurrent tasks that the processor will use when processing. @default 1 */
   maxConcurrency?: number;
+  /** The interval at which the processor will poll for tasks when previous poll returned a task @default 50ms */
   claimIntervalMs?: number;
+  /** The maximum time a task can be claimed for processing. Before it will be considered stale and claimed again @default 10000ms */
   claimStaleTimeoutMs?: number;
+  /** The interval at which the processor will idle. @default 5000ms */
   idleIntervalMs?: number;
+  /** The interval at which the processor will poll for tasks when previous poll does not return task for processing @default 5000ms */
   taskHandlerTimeoutMs?: number;
+  /** The maximum number of retries for a task handler, before task is marked as failed. @default 10 */
   taskHandlerMaxRetries?: number;
+  /** The interval at which the processor will poll for tasks when an unexpected error occurs. @default 20000ms */
   processLoopRetryIntervalMs?: number;
 };
 
