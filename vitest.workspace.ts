dfb3c5cf954920d11523a4a5c009dbc95731782c
Darren Picard <43220734+darrenpicard25@users.noreply.github.com>
2026-02-04 09:20:23 -0700

    Fix module issues with mongo-datastore (#61)
    I missed updating chrono-mongo-datastore package.json properly in last PR with tsdown update. chrono-core was updated but i missed chrono-mongo-datastore
    
    Also did some minor updates and discovered that vitest workspace vitest.workspace.ts was removed in v4. and changed to just root vitest.config.ts with projects
    
    I will deprecate version 0.5.2 once this is published

--- vitest.workspace.ts
+++
@@ -1,7 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  './packages/chrono-core/vitest.config.ts',
-  './packages/chrono-memory-datastore/vitest.config.ts',
-  './packages/chrono-mongo-datastore/vitest.config.ts',
-]);
